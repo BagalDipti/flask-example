baef43a73841138e5941078686e35fa177cb5502
Dipti Bagal <52457642+BagalDipti@users.noreply.github.com>
2021-06-19 19:59:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from tabulate import tabulate
+import pymysql
+import os
+
+db_host = os.environ['MYSQL_SERVICE_PORT_3306_TCP_ADDR']
+db_user = 'root'
+db_pass = os.environ['MYSQL_ROOT_PASSWORD']
+db_name = 'sakila'
+
+
+db = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+
+app = Flask(__name__)
+
+@app.route('/')
+def someName():
+    cursor = db.cursor()
+    sql = "select * from actor LIMIT 50"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    result_list = list(results)
+    results = [ list(element) for element in result_list ]
+    return tabulate(results, tablefmt="html")
+
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host='0.0.0.0',port='8080')
